8c323704f54343670a292bc8f912f9643aa28dfd
bloper97 <steven.ribette@epsi.fr>
2020-01-26 23:18:15 +0100

    update

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ var FIELD_WIDTH = 400;
 var FIELD_LENGTH = 400;
 var FIELD_HEIGHT = 100;
 var PONG_RADIUS = 10;
-var BAR_WIDTH = FIELD_WIDTH/5;
-var BAR_HEIGHT = FIELD_HEIGHT/5;
+var BAR_WIDTH = FIELD_WIDTH/4;
+var BAR_HEIGHT = FIELD_HEIGHT/3;
 var barSpeedX = BAR_WIDTH/2.5;
-var barSpeedY = BAR_HEIGHT/2.5;
+var barSpeedY = BAR_HEIGHT;
 var isRunning = false;
 var lifeBoard = document.getElementById('lifeBoard');
-var scoreBoard = document.getElementById('scoreBoard');
-var life = 3;
+var resultBoard = document.getElementById('resultBoard');
+var life = [7,7];
 var starTime;
 var endTime;
 var bestTime = 0;
 var HITING = 1;
+var barRotation = 0;
 
 var views = ["topView","frontView","thirdPersonView"];
 var actualView = 0;
@@ -28,6 +29,7 @@ var listener = new THREE.AudioListener();
 camera.add( listener );
 
 var pongSound = new THREE.Audio( listener );
+var pongDragonSound = new THREE.Audio( listener );
 
 var backgroundSound = new THREE.Audio( listener );
 
@@ -36,9 +38,13 @@ audioLoader.load( 'sounds/pong.wav', function( buffer ) {
 	pongSound.setBuffer( buffer );
 	pongSound.setVolume( 1 );
 });
+audioLoader.load( 'sounds/dragon.wav', function( buffer ) {
+	pongDragonSound.setBuffer( buffer );
+	pongDragonSound.setVolume( 1 );
+});
 audioLoader.load( 'sounds/background.wav', function( buffer ) {
 	backgroundSound.setBuffer( buffer );
-    backgroundSound.setVolume( 0.2 );
+    backgroundSound.setVolume( 0.1 );
     backgroundSound.play();
 });
 
@@ -80,8 +86,26 @@ group.add(bar2);
 var pongMaterial = new THREE.MeshStandardMaterial();
 var pong = new THREE.Mesh(new THREE.SphereGeometry(PONG_RADIUS,10,10), pongMaterial);
 scene.add(pong);
-var pointLight = new THREE.PointLight( 'white', 1, 100 );
-pong.add( pointLight );
+var projo1 = new THREE.SpotLight( 'white', 0.2 );
+projo1.position.set(FIELD_WIDTH/2,FIELD_HEIGHT/2,FIELD_LENGTH/2);
+projo1.lookAt(pong);
+projo1.castShadow = true;
+scene.add( projo1 );
+var projo2 = new THREE.SpotLight( 'white', 0.2 );
+projo2.position.set(FIELD_WIDTH/2,FIELD_HEIGHT/2,-FIELD_LENGTH/2);
+projo2.lookAt(pong);
+projo2.castShadow = true;
+scene.add( projo2 );
+var projo3 = new THREE.SpotLight( 'white', 0.2 );
+projo3.position.set(-FIELD_WIDTH/2,FIELD_HEIGHT/2,FIELD_LENGTH/2);
+projo3.lookAt(pong);
+projo3.castShadow = true;
+scene.add( projo3 );
+var projo4 = new THREE.SpotLight( 'white', 0.2 );
+projo4.position.set(-FIELD_WIDTH/2,FIELD_HEIGHT/2,-FIELD_LENGTH/2);
+projo4.lookAt(pong);
+projo4.castShadow = true;
+scene.add( projo4 );
 
 function axes(){
     var axesHelper = new THREE.AxesHelper( 50 );
@@ -92,7 +116,7 @@ function axes(){
     var bar2AxesHelper = new THREE.AxesHelper( 50 );
     bar2.add( bar2AxesHelper );
 }
-
+//axes();
 function topView(){
     camera.position.z = 0;
     camera.position.y = 500;
@@ -146,20 +170,42 @@ textureLoader.load("textures/rakette.jpg", function(texture) {
     bar2.material.needsUpdate = true;
     renderer.render(scene, camera);
 });
-
+function howtoplay(){
+    alert('Move with z,q,s,d');
+    alert('Hit the ball with spacebar to Smash');
+    alert('Be the last one alive');
+}
 function init(){
-    document.getElementById('center').style.display = 'none';
+    document.getElementById('startbtn').style.display = 'none';
+    document.getElementById('howtoplaybtn').style.display = 'none';
     startTime = newDate();
-    lifeBoard.innerHTML = 'Life: ' + life;
+    lifeBoard.innerHTML = 'Player 1: ' + life[0] + ' | ' + life[1] + ' : Player 2';
+    resultBoard.innerHTML = "";
     startRender();
     document.addEventListener("keydown", barMove, false);
     renderer.domElement.style.cursor = 'none';
     
 }
+function cpuProcess() {
+    let pongPos = pong.position;
+    let cpuPos = bar2.position;
+    
+    if(cpuPos.x - 100 > pongPos.x) {
+      cpuPos.x -= Math.min(cpuPos.x - pongPos.x, 6);
+    }else if(cpuPos.x - 100 < pongPos.x) {
+      cpuPos.x += Math.min(pongPos.x - cpuPos.x, 6);
+    }
+    if(cpuPos.y - 100 > pongPos.y) {
+        cpuPos.y -= Math.min(cpuPos.y - pongPos.y, 6);
+    }else if(cpuPos.y - 100 < pongPos.y) {
+        cpuPos.y += Math.min(pongPos.y - cpuPos.y, 6);
+    }
+}
 function newDate(){
     return Math.round(+new Date() / 1000);
 }
 function reset(){
+    resultBoard.innerHTML = "";
     pong.position.set(0,0,0);
     pong.$velocity = null;
 }
@@ -174,27 +220,37 @@ function render() {
     if(isRunning) {
       requestAnimationFrame(render);
       processPongMovement();
+      cpuProcess();
       renderer.render(scene, camera);
     }
 }
-function losePoint(){
-    life--;
-    lifeBoard.innerHTML = 'Life: ' + life;
+function losePoint(player){
+    life[player]--;
+    lifeBoard.innerHTML = 'Player 1 : ' + life[0] + ' | ' + life[1] + ' : Player 2';
     pong.$stopped = true;
-    if(life==0){
-        life = 3;
-        endTime = newDate();
-        let time = endTime - startTime;
-        if(time>bestTime) bestTime = time;
-        scoreBoard.innerHTML = "Best score " + bestTime + " sec";
-        if(confirm("You survived "+time+" sec, ready to play again?")){
-            startTime = newDate();
-            lifeBoard.innerHTML = 'Life: ' + life;
-            setTimeout(reset, 2000);
-        }
+    let isOver = false;
+    if(life[0]==0){
+        isOver = true;
+        resultBoard.innerHTML = "You lose";
+    }else if(life[1]==0){
+        isOver = true;
+        resultBoard.innerHTML = "You win";
     }else{
         setTimeout(reset, 2000);
     }
+    setTimeout(function(){
+        if(isOver){
+            life = [7,7];
+            endTime = newDate();
+            let time = endTime - startTime;
+            if(time>bestTime) bestTime = time;
+            if(confirm("You played "+time+" sec, ready to play again?")){
+                startTime = newDate();
+                lifeBoard.innerHTML = 'Player 1: ' + life[0] + ' | ' + life[1] + ' : Player 2';
+                setTimeout(reset, 2000);
+            }
+        }
+    }, 100);
 }
 function barCanGoUp(){
     return bar1.position.y + barSpeedY < FIELD_HEIGHT/2;
@@ -203,10 +259,10 @@ function barCanGoDown(){
     return bar1.position.y - barSpeedY > -FIELD_HEIGHT/2;
 }
 function barCanGoLeft(){
-    return bar1.position.x + barSpeedX < FIELD_WIDTH/2 -BAR_WIDTH;
+    return bar1.position.x + barSpeedX < FIELD_WIDTH/2;
 }
 function barCanGoRight(){
-    return bar1.position.x - barSpeedX > -FIELD_WIDTH/2 +BAR_WIDTH;
+    return bar1.position.x - barSpeedX > -FIELD_WIDTH/2;
 }
 function barMove(event){
     var keyCode = event.which;
@@ -214,25 +270,25 @@ function barMove(event){
         //z
         if(barCanGoUp()){
             bar1.position.y += barSpeedY;
-            bar2.position.y += barSpeedY;
+            //bar2.position.y += barSpeedY;
         }
     } else if (keyCode == 83) {
         //s
         if(barCanGoDown()){
             bar1.position.y -= barSpeedY;
-            bar2.position.y -= barSpeedY;
+            //bar2.position.y -= barSpeedY;
         }
     } else if (keyCode == 81) {
         //q
         if(camera.position.z<=0){
             if(barCanGoLeft()){
                 bar1.position.x += barSpeedX;
-                bar2.position.x += barSpeedX;
+                //bar2.position.x += barSpeedX;
             }
         }else{
             if(barCanGoRight()){
                 bar1.position.x -= barSpeedX;
-                bar2.position.x -= barSpeedX;
+                //bar2.position.x -= barSpeedX;
             }
         }
         
@@ -241,18 +297,18 @@ function barMove(event){
         if(camera.position.z>=0){
             if(barCanGoLeft()){
                 bar1.position.x += barSpeedX;
-                bar2.position.x += barSpeedX;
+                //bar2.position.x += barSpeedX;
             }
         }else{
             if(barCanGoRight()){
                 bar1.position.x -= barSpeedX;
-                bar2.position.x -= barSpeedX;
+                //bar2.position.x -= barSpeedX;
             }
         }
     }
     if (keyCode == 32) {
         //spacebar
-        coupDuDragon();
+        coupDuDragonBar1();
     }
     if(keyCode == 86){
         actualView++;
@@ -269,27 +325,39 @@ function barMove(event){
         renderer.render(scene, camera);
     }
 };
-function coupDuDragon(){
+function coupDuDragonBar1(){
     if(pong.position.x<0){
         bar1.rotateY(1.1);
-        bar2.rotateY(-1.1);
         HITING = 2;
         setTimeout(function (){
             bar1.rotateY(-1.1);
-            bar2.rotateY(1.1);
             HITING = 1;
         }, 100);
     }else{
         bar1.rotateY(-1.1);
-        bar2.rotateY(1.1);
         HITING = 2;
         setTimeout(function (){
             bar1.rotateY(1.1);
+            HITING = 1;
+        }, 100);
+    }   
+}
+function coupDuDragonBar2(){
+    if(pong.position.x<0){
+        bar2.rotateY(-1.1);
+        HITING = 2;
+        setTimeout(function (){
+            bar2.rotateY(1.1);
+            HITING = 1;
+        }, 100);
+    }else{
+        bar2.rotateY(1.1);
+        HITING = 2;
+        setTimeout(function (){
             bar2.rotateY(-1.1);
             HITING = 1;
         }, 100);
     }
-    
 }
 function startPongMovement() {
     var direction = Math.random() > 0.5 ? -1 : 1;
@@ -304,7 +372,7 @@ function updatePongPosition() {
     pong.position.x += pong.$velocity.x;
     pong.position.z += pong.$velocity.z;
     pong.position.y += pong.$velocity.y;
-    rotatePong(2);
+    rotatePong(2.2);
 }
 function rotatePong(t){
     pong.rotateY(t);
@@ -320,10 +388,14 @@ function isFloorCollision(){
     return pY - PONG_RADIUS < -fhby2 || pY + PONG_RADIUS > fhby2;
 }
 function isBar1Collision(){
-    return pong.position.z - PONG_RADIUS <= bar1.position.z && isPongXBar(bar1) && pong.position.y - PONG_RADIUS <= bar1.position.y && isPongYBar(bar1);
+    if(HITING==2) barRotation = 20;
+    setTimeout(barRotation=0,100);
+    return pong.position.z - PONG_RADIUS <= bar1.position.z + barRotation && isPongXBar(bar1) && pong.position.y - PONG_RADIUS <= bar1.position.y && isPongYBar(bar1);
 }
 function isBar2Collision(){
-    return pong.position.z + PONG_RADIUS >= bar2.position.z && isPongXBar(bar2) && pong.position.y + PONG_RADIUS >= bar2.position.y && isPongYBar(bar2);
+    if(HITING==2) barRotation = 20;
+    setTimeout(barRotation=0,100);
+    return pong.position.z + PONG_RADIUS >= bar2.position.z - barRotation && isPongXBar(bar2) && pong.position.y + PONG_RADIUS >= bar2.position.y && isPongYBar(bar2);
 }
 function isPongXBar(bar) {
     var bwby2 = BAR_WIDTH / 2;
@@ -338,16 +410,19 @@ function isPongYBar(bar) {
     return pY > bY - bhby2 && pY < bY + bhby2;
 }
 function isPastBar1(){
-    return pong.position.z < bar1.position.z - BAR_HEIGHT;
+    return pong.position.z + PONG_RADIUS < bar1.position.z;
 }
 function isPastBar2(){
-    return pong.position.z > bar2.position.z + BAR_HEIGHT;
+    return pong.position.z - PONG_RADIUS > bar2.position.z;
 }
 function hitPong(bar) {
     if(!pongSound.isPlaying){
         pongSound.play();
     }
-    pong.$velocity.x = (pong.position.x - bar.position.x) / 5 * HITING; 
+    if(HITING==2 && !pongDragonSound.isPlaying){
+        pongDragonSound.play();
+    }
+    pong.$velocity.x = (pong.position.x - bar.position.x) / 5 * HITING;
     pong.$velocity.z *= -1*HITING;
 }
 function processPongMovement() {
@@ -376,7 +451,9 @@ function processPongMovement() {
         hitPong(bar2);
     }
     
-    if(isPastBar1() || isPastBar2()){
-        losePoint();
+    if(isPastBar1()){
+        losePoint(0);
+    }else if(isPastBar2()){
+        losePoint(1);
     }
 }
